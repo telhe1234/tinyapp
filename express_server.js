6997bab0269df0b4b3ae631d5c06204314bd8694
Taha Helwani <telhe008@uottawa.ca>
2021-11-09 05:36:04 +0000

    Implemented the new form post request and wrote the code of urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -24,11 +30,18 @@ app.get("/hello", (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
-})
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
